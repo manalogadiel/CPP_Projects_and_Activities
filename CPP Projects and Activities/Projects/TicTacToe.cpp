cbbbad8e02dde61a5bd8077c11c5b51b444772c0
manalogadiel <153746453+manalogadiel@users.noreply.github.com>
2025-02-28 18:11:24 +0800

    Update TicTacToe.cpp

--- CPP Projects and Activities/Projects/TicTacToe.cpp
+++ CPP Projects and Activities/Projects/TicTacToe.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 char marker;
 char board[3][3] ={ {'1', '2', '3'}, 
-								 {'4', '5', '6'}, 
-								 {'7', '8', '9'}};
+		    {'4', '5', '6'}, 
+		    {'7', '8', '9'}};
 void theboard(){
 	cout << "-------------\n";
 	for (int i = 0; i < 3; i++){
